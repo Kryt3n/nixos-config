b4a6b44a7f77ef15c8a010fd92274323cc0a3bbf
Kryten <jmburn755@gmail.com>
2024-07-26 15:51:01 +1000

    updated flake.lock

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,8 @@
       ./modules/packages/default.nix
       ./modules/bootconfig/default.nix
     ];
-
+#  stylix.enable = true;
+#  stylix.image = /home/Kryten/wallpaper.jpg;
   programs = {
     mtr.enable = true;
     gnupg.agent = {
